323cb2d2587647cbdb6b54d333661497666cfc24
Maycon Soares <maycondouglas.soares@hotmail.com>
2024-02-25 15:54:38 -0300

    Adicionar arquivo PHP para calcular média de três números :wq

---
+++ PHP/calcular.php
@@ -0,0 +1,12 @@
+<?php
+// Definindo três variáveis para armazenar os números
+$num1 = 10;
+$num2 = 20;
+$num3 = 30;
+
+// Calculando a média dos três números
+$media = ($num1 + $num2 + $num3) / 3;
+
+// Exibindo a média
+echo "A média dos números $num1, $num2 e $num3 é: $media";
+?>
