ee537f31117733be56406d187fb860636dc87574
Maycon Soares <maycondouglas.soares@hotmail.com>
2024-02-25 15:31:04 -0300

    Adicionar script JS  separado

---
+++ HTML-CSS-JAVASCRIPT/script.js
@@ -0,0 +1,10 @@
+        
+let num1 = 10;
+let num2 = 20;
+let num3 = 30;
+
+// Calculando a média dos três números
+let media = (num1 + num2 + num3) / 3;
+
+// Exibindo a média no console
+console.log("A média dos números", num1 + ",", num2 + " e", num3 + " é:", media);
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # lista-01-udwmj-2024
-1° contato com a linguagem Javascript e PHP, utilizando HTML e CSS.
+## 1° contato com a linguagem Javascript e PHP, utilizando HTML e CSS.
